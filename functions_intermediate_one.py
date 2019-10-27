c7c7853328146071148170ae404063a1544ffcc6
Michelle Tanzil <mtanzil01@gmail.com>
2019-10-27 14:11:21 -0700

    first commit - assignment completed

---
+++ functions_intermediate_one.py
@@ -0,0 +1,24 @@
+# Write a single function, randInt() that takes up to 2 arguments.
+
+# If no arguments are provided, the function should return a random integer between 0 and 100.
+# If only a max number is provided, the function should return a random integer between 0 and the max number.
+# If only a min number is provided, the function should return a random integer between the min number and 100
+# If both a min and max number are provided, the function should return a random integer between those 2 values.
+import random
+def randInt(min=0, max=100):
+    if min > max or max < 0:
+        return False
+    else:
+        num = round(random.random() * (max - min) + min)
+        return num
+
+print(randInt())
+# should print a random integer between 0 to 100
+print(randInt(max=50))
+# should print a random integer between 0 to 50
+print(randInt(min=50))
+# should print a random integer between 50 to 100
+print(randInt(min=50, max=500))
+# should print a random integer between 50 and 500
+
+
